fd4be47a5e6560c4bc434463eeda214b3c36e88c
vitoibra93 <vitoibra93@gmail.com>
2021-10-20 21:03:49 +0700

    menghapus bagian user me items

--- main.py
+++ main.py
@@ -141,10 +141,6 @@ async def read_users_me(current_user: User = Depends(get_current_active_user)):
     return current_user
 
 
-@app.get("/users/me/items/")
-async def read_own_items(current_user: User = Depends(get_current_active_user)):
-    return [{"item_id": "Foo", "owner": current_user.username}]
-
 #Bagian Menu 2
 @app.get('/')
 def root():
